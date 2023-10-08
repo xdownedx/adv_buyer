03c36dcd23e25cb756a36c08e3e7e42d0eb338ba
Max Palehin <59222687+xdownedx@users.noreply.github.com>
2023-10-08 22:56:10 +0300

    added functional for delete channel

--- api/delete_channel.py
+++ api/delete_channel.py
@@ -1,17 +1,14 @@
 from loader import app, database, bots  # database is already imported
-from userbot import UserBot
-from database import Channel, ChannelBotRelation  # Import necessary models
 from pydantic.dataclasses import dataclass
 from typing import Optional
 from .check_public_channel import is_channel_public
-from database import Channel, ChannelBotRelation  # Import necessary models and session
+from database import Channel
 from datetime import datetime
 @dataclass
 class ChannelURL:
     url: str
 @app.post("/remove_channel")
 async def add_new_channel(body: ChannelURL):
-    # Initially checking if any bot already has access to the channel
     try:
         if is_channel_public(body.url):
             bot = min(bots.values(), key=lambda x: x.request_count)
@@ -25,9 +22,12 @@ async def add_new_channel(body: ChannelURL):
                     channel_id = await bot.check_url(url=body.url)
                     if channel_id:
                         # If any bot can access the channel, no need to subscribe
-                        channel = await bot.unsub_to_channel(channel_link=channel_id)
-                        database.delete_record(Channel, telegram_id=channel_id)
-                        return {'status': "ok", 'channel': channel}
+                        await bot.unsub_to_channel(channel_id=channel_id)
+                        try:
+                            database.delete_record(Channel, telegram_id=channel_id)
+                        except:
+                            pass
+                        return {'status': "ok", 'channel': channel_id}
                 except Exception as e:
                     return {'status': 'fail', "error":e}
             return {'status': 'fail', "error": "channel not found"}
